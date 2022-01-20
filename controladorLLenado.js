0640210dc478832aca8038d8b5225d9f22d7fba8
501 <501@academicos.cesde.edu.co>
2022-01-20 09:32:50 -0500

    se añaden fotos

---
+++ controladorLLenado.js
@@ -0,0 +1,13 @@
+let productos=[
+    {nombre:"Aguardiente tapa roja", tamaño: "1/2 Botella", precio:200000, foto:"img/foto3"},
+    {nombre:"Aguardiente tapa azul", tamaño: " 1 Botella", precio:220000, foto:"img/foto4"},
+    {nombre:"Tequila don julio",tamaño: "1 Botella", precio:300000, foto:"img/foto5"},
+    {nombre:"cerveza Aguila", tamaño: "1 Botella", precio:8000, foto:"img/foto6"},
+    {nombre:"Cerveza pilsen", tamaño: "1 Botella", precio:7000, foto:"img/foto7"},
+    {nombre:"Ron medellin", tamaño: "1/2 Botella", precio:220000, foto:"img/foto8"},
+    {nombre:"Ron añejo 220 de Caldas", tamaño: "1/2 Botella", precio:100000, foto:"img/foto9"},
+    {nombre:"Agua", tamaño: "1 Botella", precio:4000, foto:"img/foto10"},
+    {nombre:"Soda", tamaño: "1 Botella", precio:6000, foto:"img/foto11"},
+    {nombre:"Gatorade", tamaño: "1 Botella", precio:9000, foto:"img/foto12"}
+
+]
